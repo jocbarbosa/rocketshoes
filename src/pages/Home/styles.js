81132f95b5af9ab52ecadc7dfb5f4044247c9086
Joao C Barbosa <joaao@outlook.com>
2020-06-07 00:13:15 -0300

    feat: Finishing and styling HomePage

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const ProductList = styled.ul`
     display: grid;
@@ -39,5 +40,30 @@ export const ProductList = styled.ul`
         border: none;
         overflow: hidden;
         margin-top: auto;
+
+        display: flex;
+        align-items: center;
+
+        div {
+            display: flex;
+            align-items: center;
+            padding: 12px;
+            background: rgba(0,0,0,0.1);
+        }
+
+        svg {
+            margin-right: 5px;
+        }
+
+        span {
+            flex: 1;
+            text-align: center;
+            font-weight: bold;
+        }
+
+        &:hover {
+            background: ${darken(0.03, '#7159C1')};
+            transition: background 0.5s;
+        }
     }
 `;
