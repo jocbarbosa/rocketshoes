d7a7942210e04f418e8e5d692843808cb98376ca
Joao C Barbosa <joaao@outlook.com>
2020-06-09 23:35:13 -0300

    feat: Add redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import GlobalStyle from './styles/global';
 import Header from './components/Header/';
-
 import Routes from './routes.js';
+
+import store from './store';
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Header />
-            <Routes />
-            <GlobalStyle />
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Header />
+                <Routes />
+                <GlobalStyle />
+            </BrowserRouter>
+        </Provider>
+
     );
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../utils/format';
 
 import { api } from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
 
     state = {
         products: []
@@ -14,7 +16,21 @@ export default class Home extends Component {
     async componentDidMount() {
         const response = await api.get('products');
 
-        this.setState({ products: response.data });
+        const data = response.data.map(product => ({
+            ...product,
+            priceFormated: formatPrice(product.price)
+        }));
+
+        this.setState({ products: data });
+    }
+
+    handleAddProduct(product) {
+        const { dispatch } = this.props;
+
+        dispatch({
+            type: 'ADD_TO_CART',
+            product
+        })
     }
 
     render() {
@@ -22,15 +38,14 @@ export default class Home extends Component {
         const { products } = this.state;
         return (
             <ProductList>
-
                 {
                     products.map(product => (
                         <li key={product.id}>
-                            <img src={product.image} />alt={product.title} />
+                            <img src={product.image} alt={product.title} />
                             <strong>{product.title}</strong>
-                            <span>{product.price}</span>
+                            <span>{product.priceFormated}</span>
 
-                            <button type="button">
+                            <button type="button" onClick={() => this.handleAddProduct(product)}>
                                 <div>
                                     <MdAddShoppingCart size={16} color="#FFF" />
                                 </div>
@@ -45,3 +60,5 @@ export default class Home extends Component {
     }
 
 }
+
+export default connect()(Home);
