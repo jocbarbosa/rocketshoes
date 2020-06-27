f6404a3c73819aa6db0c61995a1d0877d18ef1b3
Joao C Barbosa <joaao@outlook.com>
2020-06-26 22:27:37 -0300

    feat: Add toastify

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,4 +1,5 @@
 import { call, select, put, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import api from '../../../services/api';
 import { formatPrice } from '../../../utils/format';
@@ -18,7 +19,7 @@ function* addToCart({ id }) {
     const amount = currentAmount + 1;
 
     if (amount > stockAmount) {
-        console.log('PRODUTO ESGOTADO');
+        toast.error('Produto sem estoque');
         return;
     }
 
